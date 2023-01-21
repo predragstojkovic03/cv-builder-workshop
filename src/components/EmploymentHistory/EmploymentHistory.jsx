8858a98bf125221f8c9f7ef3e28943cd68bfa007
Predrag Stojković <predragstojkovic.jagodina@gmail.com>
2023-01-21 22:56:01 +0100

    Started working on DragAndDropList for Employment History. WIP

--- src/components/EmploymentHistory/EmploymentHistory.jsx
+++ src/components/EmploymentHistory/EmploymentHistory.jsx
@@ -4,6 +4,7 @@ const EmploymentHistory = () => {
   return (
     <div className='employmentHistoryWrapper'>
       <h2>Personal Details</h2>
+      <DragAndDropList></DragAndDropList>
     </div>
   );
 };
